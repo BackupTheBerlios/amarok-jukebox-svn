ed5cb1a3299de53225bb2f6f55029225ad6264ea
hugo <hugo@f3414a77-ccf4-0310-8940-8758cd1ef58c>
2005-04-15 18:42:03 +0000

    Ready to move

--- cgi-bin/CGI.py
+++ cgi-bin/CGI.py
@@ -1,5 +1,5 @@
 import sys, os.path
-sys.path += [ os.path.abspath(os.path.basename(sys.argv[0]) + '/..') ]
+sys.path += [ os.path.abspath(os.path.basename(sys.argv[0]) + '/../..') ]
 import cgitb; cgitb.enable()
 
 def httpHeaders():
@@ -8,12 +8,16 @@ def httpHeaders():
     print
 
 def htmlHead():
-    print "<html xmlns='http://www.w3.org/1999/xhtml'>"
+    print """<?xml version=\"1.0\"?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
+\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"""
+    print "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>"
     print """<head>
-<title>Amarok Mserv</title>
+<title>amaroK juKebok</title>
 </head>""" 
     print "<body>"
 
 def htmlTail():
+    print "<hr /><addressing><a href='..'>Home</a></address>"
+    print "</body>"
     print "</html>"
-
--- cgi-bin/browse.py
+++ cgi-bin/browse.py
@@ -10,7 +10,7 @@ from Collection import Collection
 def artistsHtml(c):
     s = "<ul>"
     for id, name in c.artists():
-        s += "<li><a href='/cgi-bin/browse.py?artist=%s'>%s</a></li>" % (id , cgi.escape(name))
+        s += "<li><a href='/cgi-bin/browse.py?artist=%s'>%s</a></li>" % (id , cgi.escape(name.encode('utf-8')))
         s += "</ul>"
     return s
 
@@ -18,7 +18,7 @@ def albumsByArtistHtml(c, id):
     s = "<form action='/cgi-bin/playlist.py' method='post'>"
     s = "<ul>"
     for id, name in c.albumsByArtist(id):
-        s += "<li><input type='checkbox' name='album' value='%s' /> <a href='/cgi-bin/browse.py?album=%s'>%s</a></li>" % (id, id , cgi.escape(name))
+        s += "<li><input type='checkbox' name='album' value='%s' /> <a href='/cgi-bin/browse.py?album=%s'>%s</a></li>" % (id, id , cgi.escape(name.encode('utf-8')))
     s += "</ul>"
     s += "<input type='submit' name='addAlbums' value='Queue selected entire albums' />"
     s += "</form>"
@@ -28,7 +28,7 @@ def songsByAlbumHtml(c, id):
     s = "<form action='/cgi-bin/playlist.py' method='post'>"
     s += "<ol>"
     for url, title in c.songsByAlbum(id):
-        s += "<li><input type='checkbox' name='song' value='%s' /> <a href='/cgi-bin/browse.py?song=%s'>%s</a></li>" % (cgi.escape(url), urllib.quote(url), cgi.escape(title))
+        s += "<li><input type='checkbox' name='song' value='%s' /> <a href='/cgi-bin/browse.py?song=%s'>%s</a></li>" % (cgi.escape(url.encode('utf-8')), urllib.quote(url.encode('utf-8')), cgi.escape(title.encode('utf-8')))
     s += "</ol>"
     s += "<input type='submit' name='addSongs' value='Queue selected songs' />"
     s += "</form>"
@@ -44,7 +44,7 @@ def main():
 
     if os.environ['REQUEST_METHOD'] == "GET":
         if form.has_key('artists'):
-            print artistsHtml(c).encode('utf-8')
+            print artistsHtml(c)
         elif form.has_key('artist'):
             print albumsByArtistHtml(c, form['artist'].value)
         elif form.has_key('album'):
