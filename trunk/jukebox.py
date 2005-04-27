aa40956ff72bea89e8b9dade993fd4ce28764e53
hugo <hugo@f3414a77-ccf4-0310-8940-8758cd1ef58c>
2005-04-27 23:54:29 +0000

    Removed useless(?) sys.getdefaultencoding call

--- trunk/jukebox.py
+++ trunk/jukebox.py
@@ -13,7 +13,6 @@ class Jukebox:
     def __exit_signal_handler(self, signal, frame):
         print "Exiting..."
         self.__eventHandler.stop()
-        sys.getdefaultencoding = lambda: 'utf-8' 
         sys.exit(0)
 
     def __init__(self, port = -1):
