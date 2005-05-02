9d97c9594ea8e5c163997c13368dc06046f8ad76
hugo <hugo@f3414a77-ccf4-0310-8940-8758cd1ef58c>
2005-05-02 19:41:00 +0000

    Added version info to SVN (doh!)

---
+++ trunk/Version.py
@@ -0,0 +1,14 @@
+import CGI
+
+Version = "0.5.1"
+
+def serve(request):
+    doc = CGI.httpHeaders()
+    doc += CGI.htmlHead()
+
+    doc += "<p>This is amaroK-juKebox " + Version + ".</p>"
+    doc += "<p>For more recent versions, see the <a href='http://amarok-jukebox.berlios.de/'>home page</a>.</p>"
+
+    doc += CGI.htmlTail()
+
+    request.serve_string(doc)
