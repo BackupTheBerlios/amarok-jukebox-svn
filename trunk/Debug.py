ad4af9089e4580cc6ee19512dbc4affab6affd49
hugo <hugo@f3414a77-ccf4-0310-8940-8758cd1ef58c>
2005-05-02 19:42:23 +0000

    Disabled logging

--- trunk/Debug.py
+++ trunk/Debug.py
@@ -1,10 +1,12 @@
 import datetime
 
-# This needs to be made configurable
+# FIXME: This needs to be made configurable
 
 logfile = '/tmp/amarok-jukebox.log'
 
 def log(s):
+    # Disabled for release
+    return
     f = open(logfile, 'a')
     if f == None:
         return
